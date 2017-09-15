cac1bb3bd72c15b97de6bcab5213df9a89379d84
Hemant Mangwani <hemantmangwani99@gmail.com>
2017-09-15 08:27:25 +0530

    First Program in JAVA

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/**
+*Program of Hello world
+*@author Hemant Mangwani
+*@param nothing
+*/
+public class HelloWorld
+{
+  public static void main(String args[])
+  {
+   System.out.println("Hello World");
+  }
+}
